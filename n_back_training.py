24c53e2959b4be5aaa5a333e6eb090f53ec4d9f8
Complexology <lnmantlo@gmail.com>
2024-06-09 15:53:47 -0500

    initial project setup

---
+++ n_back_training.py
@@ -0,0 +1,14 @@
+import time
+import random
+import math
+import tkinter as tk
+from tkinter import Canvas, Tk
+
+def main():
+    print('hello world')
+
+
+
+
+if __name__ == '__main__':
+    main()
