cd4c267d0afd52eba9fb8927669f990b3ced393f
Complexology <lnmantlo@gmail.com>
2024-06-22 15:27:10 -0500

    Cleans up code and reorganizes sections

--- NBack Pygame/NbackPygame.py
+++ NBack Pygame/NbackPygame.py
@@ -7,7 +7,7 @@ import sys
 import pygame_widgets as widg   
 from pygame_widgets.button import Button
 
-#LAYOUT CONSTANTS
+####LAYOUT CONSTANTS#####
 CANVAS_WIDTH = 500
 CANVAS_HEIGHT = 700
 #grid
@@ -15,9 +15,6 @@ GRID_HEIGHT=400
 GRID_WIDTH=400
 GRID_GAP=10
 GRID_NUMBER=3
-
-GAME_SPEED=100
-
 #vertical padding values (CANVAS_HEIGHT=CANVAS_BOTTOM_PADDING+CANVAS_TOP_PADDING+MIDDLE_BUFFER+TOOLBOX_HEIGHT+GRID_HEIGHT)
 CANVAS_BOTTOM_PADDING=(CANVAS_HEIGHT-GRID_HEIGHT)*0.05
 CANVAS_TOP_PADDING=(CANVAS_HEIGHT-GRID_HEIGHT)*0.05
@@ -27,44 +24,67 @@ TOOLBOX_HEIGHT=(CANVAS_HEIGHT-GRID_HEIGHT)*0.80
 #horizontal padding values (CANVAS_WIDTH=CANVAS_PADDING)
 CANVAS_SIDE_PADDING=10
 
+########COLOR CONSTANTS########
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 BUTTON_DEFAULT=(200, 50, 0)
 BUTTON_HOVER=(150, 0, 0)
 BUTTON_CLICK=(0, 200, 20)
-
+CORRECT=(0, 200, 20)
+INCORRECT=(200,0,20)
+##########GAME SETTINGS#########
 NBACK_NUMBER=1
-
+GAME_SPEED=100
+#########INSTANTIATIONS######
+# running=True
+past_letters=[]
+# correct_ls=[]
+past_index=[]
+# correct_place=0
+# correct_letter=0
+######SETUP CANVAS & WINDOW#####
 render.initialize_pygame()
 screen=render.canvas_setup()
-running=True
-past_letters=[]
-correct_ls=[]
-past_index=[]
-correct_place=0
-correct_letter=0
-#start_button = render.Button(100, 100, 200, 50, (100, 100, 100), "Click me!", 30)
-args=[]
-start_button=render.button_widget(30,80,440,50,"START",20,5,BUTTON_DEFAULT,BUTTON_HOVER,BUTTON_CLICK,0,render.start_game,args)
-started=False
-#letter_button=render.button_widget(270,150,200,50,"LETTER",20,5,BUTTON_DEFAULT,BUTTON_HOVER,BUTTON_CLICK,0,render.letter_check,args)
+######INITIALIZE BUTTONS#######
+# args=[]
+# start_button=render.button_widget(30,80,440,50,"START",20,5,BUTTON_DEFAULT,BUTTON_HOVER,BUTTON_CLICK,0,render.start_game,args)
+# started=False
+# letter_args=[]
+# letter_button=render.button_widget(270,150,200,50,"LETTER",20,5,BUTTON_DEFAULT,BUTTON_HOVER,BUTTON_CLICK,0,render.letter_check,letter_args)
 #position_button=render.button_widget()
 
+######START MAIN GAME LOOP########
 def main():
     """Main game loop"""
     clock = pygame.time.Clock()
-    pygame.display.set_caption("Button Test")
+    ######VARIABLES TO BE RESET EACH LOOP######
     next_game_update_time=0
     letter=None
     index=0
     index_flg=0
     letter_flg=0
+    running=True
+    correct_ls=[]
+    correct_place=0
+    correct_letter=0
+    ######INITIALIZE BUTTONS#######
+    args=[]
+    start_button=render.button_widget(30,80,440,50,"START",20,5,BUTTON_DEFAULT,BUTTON_HOVER,BUTTON_CLICK,0,render.start_game,args)
+    started=False
+    letter_args=[]
+    letter_button=render.button_widget(270,150,200,50,"LETTER",20,5,BUTTON_DEFAULT,BUTTON_HOVER,BUTTON_CLICK,0,render.letter_check,letter_args)
+    #position_button=render.button_widget()
+
     while True:
+
+        ###SETUP TIME FOR NEXT UPDATE AND SET UPDATE FLAG WHEN IT HAS PASSED###
         current_time=time.time()
         game_update=False
         if current_time>next_game_update_time:
             next_game_update_time=current_time+(GAME_SPEED/100)
             game_update=True
+
+
         events = pygame.event.get()
         for event in events:
             if event.type == pygame.QUIT:
@@ -73,15 +93,13 @@ def main():
 
         # Clear the screen
         screen.fill(WHITE)
-        #letter_button.draw()
-        start_button.draw()
-        #position_button.draw()
-        widg.update(events)
+        
          
-        # Draw your GUI elements here
+        ################ Draw your GUI elements here########################
         Header=render.create_centered_text("N-Back Trainer","header",None,"X")
         boxes=create_grid()
-        ######slower event
+
+        ######SLOWER EVENT LOOP THAT STARTS WITH SATRT BUTTON CLICK########
         if render.started:
             if game_update:
                 return_index=get_random_index()
@@ -90,82 +108,57 @@ def main():
                 return_letter=get_random_letter()
                 letter=return_letter[0]
                 letter_flg=return_letter[1]
+                letter_args=[correct_letter]
+                letter_button=render.button_widget(270,150,200,50,"LETTER",20,5,BUTTON_DEFAULT,BUTTON_HOVER,BUTTON_CLICK,0,render.letter_check,letter_args)
+
+        ######RENDERED BEFORE GAME STARTED AND AT 60FPS###########
         Letter=render.create_centered_text(letter,"letter",boxes[index],"Y")
-        Position_flg=render.create_text(10,80,f"Position: {index_flg}","button")
-        Letter_flg=render.create_text(10,100,f"Letter: {letter_flg}","button")
-        
-        #start_button.draw(screen)
+        Position_flg=render.create_text(10,120,f"Position: {index_flg}","button")
+        Letter_flg=render.create_text(10,140,f"Letter: {letter_flg}","button")
+        render.letter_check(letter_args)
+        #letter_button.draw()
+        #start_button.draw()
+        #position_button.draw()    
 
 
         mouse_pos = pygame.mouse.get_pos()
         mouse_pressed = pygame.mouse.get_pressed()
-        #if start_button.is_clicked(mouse_pos, mouse_pressed):
-            #print("Button clicked!")
 
-        # for event in pygame.event.get():
-        #     start_button.handle_event(event)
-        # start_button.draw(screen)
-        # start_button.animate_click()
-        #pygame.display.flip()
-        #pygame.time.Clock().tick(60)
+        #Prints the widgets#
+        widg.update(events)
 
         # Update the display
         pygame.display.flip()
 
         # Cap the frame rate
-        clock.tick(60)
+        clock.tick(30)
 
-    #render.bind_mouse()
-    #render.bind_func_to_window_close(set_running_false)
-    ##boxes=create_grid()
-    #print_letters("A",boxes[0])
-    ##for box in boxes:
-        ##print_letters("A",box)
-    #text_id=render.create_text(10,10,"A","Arial",20)
-    #print(render.get_text_width(text_id))
-    ##render.create_text(250,25,"N-Back Trainer",'header')
-
-    ##time.sleep(1)
-
-    ##render.setup_mainloop()
 
 def start_game():
     started=True
 
 def set_running_false():
-    #says that you're modifying the global variable running when making modifications (must instantiate first)
     global running
     running = False
     #destroy canvas
     render.destroy_canvas()
 
-def get_grid_position():
+def get_grid_positions():
     box_width=((GRID_WIDTH-(GRID_GAP*(GRID_NUMBER-1)))/GRID_NUMBER)
     box_height=((GRID_HEIGHT-(GRID_GAP*(GRID_NUMBER-1)))/GRID_NUMBER)
     upper_x=(CANVAS_WIDTH/2)-(GRID_WIDTH/2)
     middle_x=upper_x+box_width+GRID_GAP
     bottom_x=middle_x+box_width+GRID_GAP
     upper_y=CANVAS_TOP_PADDING+TOOLBOX_HEIGHT+MIDDLE_BUFFER
-    #print(CANVAS_TOP_PADDING,CANVAS_BOTTOM_PADDING,TOOLBOX_HEIGHT,MIDDLE_BUFFER,"Padding")
     middle_y=upper_y+box_height+GRID_GAP
     bottom_y=middle_y+box_height+GRID_GAP
     return upper_x,middle_x,bottom_x,upper_y,middle_y,bottom_y
 
 def create_grid():
-    x_coords=get_grid_position()[0:3]
-    #print(x_coords,"X_coords set")
-    y_coords=get_grid_position()[3:6]
-    #print(y_coords,"Y_coords set")
-    #x_coord=get_grid_position()[0]
-    #print(x_coord,"Xcoord")
-    #y_coord=get_grid_position()[5]
-    #print(y_coord,"Ycoord")
+    x_coords=get_grid_positions()[0:3]
+    y_coords=get_grid_positions()[3:6]
     box_width=((GRID_WIDTH-(GRID_GAP*(GRID_NUMBER-1)))/GRID_NUMBER)
     box_height=((GRID_HEIGHT-(GRID_GAP*(GRID_NUMBER-1)))/GRID_NUMBER)
-    #box=render.draw_rectangle(x_coord,y_coord,x_coord+box_width,y_coord+box_height,"black")
-    #check=render.get_coords(box)
-    #print(check,"box coords")
-
     boxes=[]
     for x_coord in x_coords:
         for y_coord in y_coords:
@@ -174,7 +167,6 @@ def create_grid():
     return boxes
 
 def print_letters(letter,container):
-    #text_id=render.create_text(10,10,letter)
     text_id=render.create_letter(10,10,letter,'letter')
     print(text_id)
     render.set_text_to_center(text_id,container)
@@ -193,7 +185,6 @@ def get_random_letter():
     global correct_letter
     rand_repeat=random.randint(0,5)
     random_upper_letter = chr(random.randint(ord('A'), ord('Z')))
-
     correct_letter=0
     if rand_repeat==3 and len(past_letters)>NBACK_NUMBER:
         letter=past_letters[-NBACK_NUMBER]
@@ -207,9 +198,6 @@ def get_random_letter():
     #correct_ls.append(correct_letter)
     return letter,correct_letter
 
-# def letter_check():
-#     if correct_letter==1:
-#         render.draw_rectangle()
 
 
 if __name__ == '__main__':
--- NBack Pygame/NbackPygameRenderer.py
+++ NBack Pygame/NbackPygameRenderer.py
@@ -12,9 +12,11 @@ TEXT_B1_SIZE=30
 TEXT_B1_STYLE='Arial'
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
+CORRECT=(0, 200, 20)
+INCORRECT=(200,0,20)
 screen=None
 started=False
-
+####ACTUALLY USED IN NBACK#####
 def initialize_pygame():
     pygame.init()
 
@@ -36,25 +38,6 @@ def draw_rectangle(upper_x, upper_y, lower_x, lower_y, color):
 def draw_circle(upper_x, upper_y, lower_x, lower_y, color):
     # Draw a circle on the screen
     pygame.draw.circle(screen, color, (upper_x, upper_y), (lower_x - upper_x) / 2)
-
-def obj_move_to(obj_to_move, x, y):
-    # Move the object to the specified position
-    obj_to_move.rect.x = x
-    obj_to_move.rect.y = y
-
-def obj_move(obj_to_move, delta_x, delta_y):
-    # Move the object by the specified amount
-    obj_to_move.rect.x += delta_x
-    obj_to_move.rect.y += delta_y
-
-def get_coords(obj_for_coords):
-    # Get the coordinates of the object
-    return obj_for_coords.rect.x, obj_for_coords.rect.y
-
-def get_center_coords(obj):
-    x = obj.rect.x + obj.rect.width / 2
-    y = obj.rect.y + obj.rect.height / 2
-    return x, y
 
 def create_text(x, y, words, type):
     font_to_use = get_font(type)
@@ -78,22 +61,6 @@ def create_centered_text(words, type,container,axis):
             screen.blit(text_surface, text_rect)
     return text_surface
 
-def get_width(object):
-    # Get the width of the object
-    return object.rect.width
-
-def get_height(object):
-    # Get the height of the object
-    return object.rect.height
-
-def get_text_width(text):
-    # Get the width of the text
-    return font.size(text)[0]
-
-def get_text_height(text):
-    # Get the height of the text
-    return font.size(text)[1]
-
 def get_font(type):
     # Get the font for the specified type
     if type == 'header':
@@ -105,50 +72,13 @@ def get_font(type):
     else:
         return pygame.font.SysFont("Arial", 30)
     
-def set_text_to_center(object, container):
-    # Set the text to the center of the container
-    if container is None:
-        object_rect = object.get_rect(center=screen.get_rect().center)
-        screen.blit(object, object_rect)
-    
-    
-    #object_rect = object.get_rect()
-    #if container is None:
-    
-        # center_x = CANVAS_WIDTH / 2
-        # center_y = CANVAS_HEIGHT / 2
-        # object_rect.centerx = center_x
-        # object_rect.centery = center_y
-        # object.rect = object_rect
-    # else:
-    #     container_rect = container.rect
-    #     object_rect.centerx = container_rect.centerx
-    #     object_rect.centery = container_rect.centery
-    #     object.rect = object_rect
-
-def delete_object(obj_to_delete):
-    # Remove the object from the list of sprites
-    all_sprites.remove(obj_to_delete)
-    # Or set its visible attribute to False
-    obj_to_delete.visible = False
-
-def update_mouse_coords(event):
-    global mouse_x, mouse_y
-    mouse_x, mouse_y = event.pos
-
 def destroy_canvas():
     pygame.quit()
     sys.exit()
 
-def find_overlaps(upper_x, upper_y, lower_x, lower_y):
-    overlaps = []
-    for obj in all_sprites:
-        if (upper_x <= obj.rect.x <= lower_x and
-            upper_y <= obj.rect.y <= lower_y):
-            overlaps.append(obj)
-    return overlaps
+##### NBACK BUTTONS FUNCTIONS #####
 
-def button_widget(x,y,width,height,text,font_size,margin,default_color,hover_color,pressed_color,radius,on_click,on_click_parameters,):
+def button_widget(x,y,width,height,text,font_size,margin,default_color,hover_color,pressed_color,radius,on_click,on_click_parameters):
     button = Button(
         # Mandatory Parameters
         screen,  # Surface to place button on
@@ -170,80 +100,80 @@ def button_widget(x,y,width,height,text,font_size,margin,default_color,hover_col
         )
     return button
 
-
 def start_game(args):
     global started
     print('Start') 
     started=True
 
+def letter_check(correct_letter):
+    if len(correct_letter)==0:
+        return
+    if correct_letter[0]==1:
+        draw_rectangle(250,140,480,200,CORRECT)
+        #print("correct")
+    elif correct_letter[0]==0:
+        draw_rectangle(250,140,480,200,INCORRECT)
+        #print("incorrect")
+    #else:
+        #print("no val")
 
+#####MIGHT BE USEFUL BUT NOT USED######
 
-# class Button:
-#     def __init__(self, x, y, width, height, color, text, font_size):
-#         self.x = x
-#         self.y = y
-#         self.width = width
-#         self.height = height
-#         self.color = color
-#         self.text = text
-#         self.font = pygame.font.SysFont("Arial", 20)
-#         self.clicked = False
-#         self.original_width = width  # Store the original width
-#         self.original_height = height  # Store the original height
+def obj_move_to(obj_to_move, x, y):
+    # Move the object to the specified position
+    obj_to_move.rect.x = x
+    obj_to_move.rect.y = y
 
-#     def draw(self, screen):
-#         pygame.draw.rect(screen, self.color, (self.x, self.y, self.width, self.height))
-#         font = self.font
-#         #pygame.font.SysFont(None, self.font_size)
-#         text_surface = font.render(self.text, True, (255, 255, 255))
-#         screen.blit(text_surface, (self.x + (self.width / 2) - (text_surface.get_width() / 2), self.y + (self.height / 2) - (text_surface.get_height() / 2)))
+def obj_move(obj_to_move, delta_x, delta_y):
+    # Move the object by the specified amount
+    obj_to_move.rect.x += delta_x
+    obj_to_move.rect.y += delta_y
 
-#     def is_hovered(self, mouse_pos):
-#         return self.x <= mouse_pos[0] <= self.x + self.width and self.y <= mouse_pos[1] <= self.y + self.height
+def get_coords(obj_for_coords):
+    # Get the coordinates of the object
+    return obj_for_coords.rect.x, obj_for_coords.rect.y
 
-#     def is_clicked(self, mouse_pos, mouse_pressed):
-#         return self.is_hovered(mouse_pos) and mouse_pressed[0]
-    
-#     # def animate_click(self):
-#     #     if self.clicked:
-#     #         self.color = (200, 200, 200)  # Change the color to a lighter shade
-#     #         if self.width>= self.original_width-50:
-#     #             self.width -= 5  # Shrink the button slightly
-#     #             self.height -= 5
-#     #     else:
-#     #         self.color = (100, 100, 100)  # Change the color back to the original
-#     #         if self.width<self.original_width:
-#     #             self.width+=5
-#     #             self.height+=5
+def get_center_coords(obj):
+    x = obj.rect.x + obj.rect.width / 2
+    y = obj.rect.y + obj.rect.height / 2
+    return x, y
 
-#     # def animate_click(self):
-#     #     if self.clicked:
-#     #         self.animation_state = "shrinking"
-#     #     else:
-#     #         self.animation_state = "growing"
+def get_width(object):
+    # Get the width of the object
+    return object.rect.width
 
-#     #     if self.animation_state == "shrinking":
-#     #         if self.width > 20:  # Minimum width
-#     #             self.width -= 5
-#     #             self.height -= 5
-#     #         else:
-#     #             self.animation_state = "done"
-#     #     elif self.animation_state == "growing":
-#     #         if self.width < self.original_width:
-#     #             self.width += 5
-#     #             self.height += 5
-#     #         else:
-#     #             self.animation_state = "done"
+def get_height(object):
+    # Get the height of the object
+    return object.rect.height
 
-#     def handle_event(self, event):
-#         if event.type == pygame.MOUSEBUTTONDOWN:
-#             if self.x <= event.pos[0] <= self.x + self.width and self.y <= event.pos[1] <= self.y + self.height:
-#                 self.clicked = True
-#         elif event.type == pygame.MOUSEBUTTONUP:
-#             if self.clicked:
-#                 self.clicked = False
-#                 # Call the on_click function here
-#                 self.on_click()
+def get_text_width(text):
+    # Get the width of the text
+    return font.size(text)[0]
 
-#     def on_click():
-#         print("Button clicked!")
+def get_text_height(text):
+    # Get the height of the text
+    return font.size(text)[1]
+
+def set_object_to_center(object, container):
+    # Set the text to the center of the container
+    if container is None:
+        object_rect = object.get_rect(center=screen.get_rect().center)
+        screen.blit(object, object_rect)
+    else:
+        object_rect = object.get_rect(center=container.get_rect().center)
+        screen.blit(object,object_rect)
+
+def delete_object(obj_to_delete):
+    del obj_to_delete
+
+def update_mouse_coords(event):
+    global mouse_x, mouse_y
+    mouse_x, mouse_y = event.pos
+
+def find_overlaps(upper_x, upper_y, lower_x, lower_y):
+    overlaps = []
+    for obj in all_sprites:
+        if (upper_x <= obj.rect.x <= lower_x and
+            upper_y <= obj.rect.y <= lower_y):
+            overlaps.append(obj)
+    return overlaps
